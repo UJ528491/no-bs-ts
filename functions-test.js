c5e19c225e70295cacec5a5f312eac17106b6fec
UJ528491 <hyj0829@gmail.com>
2021-08-02 02:33:58 +0900

    #2 - Typescript Functions

---
+++ functions-test.js
@@ -0,0 +1,6 @@
+"use strict";
+exports.__esModule = true;
+var functions_1 = require("./functions");
+console.log(functions_1["default"](1, 2));
+console.log(functions_1.addStrings("a", "b"));
+console.log(functions_1.addStrings("a"));
---
+++ functions-test.ts
@@ -0,0 +1,5 @@
+import addNumbers, { addStrings, format } from "./functions";
+
+console.log(addNumbers(1, 2));
+console.log(addStrings("a", "b"));
+console.log(addStrings("a"));
