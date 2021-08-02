9dec310ad4a31f36459f9974781b84bf59db27cc
UJ528491 <hyj0829@gmail.com>
2021-08-02 14:04:55 +0900

    #3 - Typescript Functions with Functions

---
+++ funcs-and-funcs.ts
@@ -0,0 +1,25 @@
+export function printToFile(text: string, callback: () => void): void {
+  console.log(text);
+  callback();
+}
+
+type MutationFunction = (v: number) => number;
+
+export function arrayMutate(
+  numbers: number[],
+  mutate: MutationFunction
+): number[] {
+  return numbers.map(mutate);
+}
+
+const myNewMutationFunc: MutationFunction = (v: number) => v * 100;
+
+console.log(arrayMutate([1, 2, 3], v => v * 10));
+
+export type AdderFunction = (v: number) => number;
+export function createAdder(num: number): AdderFunction {
+  return (val: number) => num + val;
+}
+
+const addOne = createAdder(1);
+console.log(addOne(55));
